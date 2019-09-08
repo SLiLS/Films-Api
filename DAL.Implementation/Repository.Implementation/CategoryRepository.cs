d3349fa5c0ff9392f7bb674f353af5b4b4c6147a
Stepan <rafalovich_99@mail.ru>
2019-09-08 17:48:39 +0300

    repositories added

---
+++ DAL.Implementation/Repository.Implementation/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Interfaces.Repository.Interfaces;
+using DAL.Interfaces.Models;
+using System.Threading.Tasks;
+
+namespace DAL.Implementation.Repository.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+     
+    }
+}
---
+++ DAL.Implementation/Repository.Implementation/FilmRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Implementation.Repository.Implementation
+{
+  public  class FilmRepository
+    {
+    }
+}
